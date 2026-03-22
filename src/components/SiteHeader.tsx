b1d9829ade37952c7c2c19aedc4db9c9760ad45f
sivammarwar <shivamkumarsingh8544@gmail.com>
2026-03-22 13:34:20 +0530

    deploying final 31

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -69,7 +69,7 @@ const SiteHeader = () => {
               alt="Hidden Facts"
               width={102}
               height={112}
-              className="h-16 w-auto object-contain"
+              className="h-12 w-auto object-contain"
               fetchPriority="high"
             />
             <div className="flex flex-col leading-tight">
