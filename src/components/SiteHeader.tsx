d20b7aa4f58dfe71855de5a7062946fbc8e18ac9
sivammarwar <shivamkumarsingh8544@gmail.com>
2026-03-22 14:01:38 +0530

    deploying final 36

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -66,10 +66,10 @@ const SiteHeader = () => {
             */}
             <img
               src="/logo.webp"
-              alt="Hidden Facts"
+              alt="Hidden Facts logo"
               width={132}
               height={145}
-              className="h-12 w-16 object-contain"
+              className="h-12 w-[44px] object-contain"
               fetchPriority="high"
             />
             <div className="flex flex-col leading-tight">
